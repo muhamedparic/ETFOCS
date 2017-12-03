f073f605f23f59d350b520fbe1a9c22904e034fc
muhamedparic <mparic1@etf.unsa.ba>
2017-12-03 18:43:56 +0100

    nesto

--- backend/main.py
+++ backend/main.py
@@ -71,4 +71,4 @@ def api_competition_list():
 	return db.competition_list()
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.20', port=8000, processes=4)
+    app.run(host='localhost', port=8000, processes=4)
